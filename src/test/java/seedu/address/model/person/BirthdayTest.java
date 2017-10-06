a38d6387e38387db3bb0f59336b956a0d02fe5b9
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-10-06 18:59:31 +0800

    Added BirthdayTest to test for valid birthday strings

---
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -0,0 +1,23 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BirthdayTest {
+
+    @Test
+    public void isValidBirthday() {
+        // invalid birthdays
+        assertFalse(Phone.isValidPhone("")); // empty string
+        assertFalse(Phone.isValidPhone(" ")); // spaces only
+        assertFalse(Phone.isValidPhone("91")); // less than 8 numbers
+        assertFalse(Phone.isValidPhone("phone")); // non-numeric
+        assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
+        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
+
+        // valid birthdays
+        assertTrue(Phone.isValidPhone("02031990")); // exactly 8 numbers
+    }
+}
