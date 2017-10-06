4535e69b25eb6cace1d3cc221ed10e7c7d5aff69
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-10-06 22:23:44 +0800

    Minor changes to Birthday test

--- src/test/java/seedu/address/model/person/BirthdayTest.java
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -10,15 +10,15 @@ public class BirthdayTest {
     @Test
     public void isValidBirthday() {
         // invalid birthdays
-        assertFalse(Phone.isValidPhone("")); // empty string
-        assertFalse(Phone.isValidPhone(" ")); // spaces only
-        assertFalse(Phone.isValidPhone("91")); // less than 8 numbers
-        assertFalse(Phone.isValidPhone("phone")); // non-numeric
-        assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
-        assertFalse(Phone.isValidPhone("02031990")); //exactly 8 digits
+        assertFalse(Birthday.isValidBirthday("")); // empty string
+        assertFalse(Birthday.isValidBirthday(" ")); // spaces only
+        assertFalse(Birthday.isValidBirthday("91")); // less than 8 numbers
+        assertFalse(Birthday.isValidBirthday("phone")); // non-numeric
+        assertFalse(Birthday.isValidBirthday("9011p041")); // alphabets within digits
+        assertFalse(Birthday.isValidBirthday("9312 1534")); // spaces within digits
+        assertFalse(Birthday.isValidBirthday("02031990")); //exactly 8 digits
 
         // valid birthdays
-        assertTrue(Phone.isValidPhone("02.03.1990")); // DD.MM.YYYY format consists of 8 digits and 2 '.'
+        assertTrue(Birthday.isValidBirthday("02.03.1990")); // DD.MM.YYYY format consists of 8 digits and 2 '.'
     }
 }
