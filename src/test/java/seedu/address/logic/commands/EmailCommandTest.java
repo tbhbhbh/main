beaaf0f28e9a23130134705b67b4f60768df4fb1
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-10-18 17:32:14 +0800

    Style fixes

--- src/test/java/seedu/address/logic/commands/EmailCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EmailCommandTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
