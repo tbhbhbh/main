98783b92cd5687995c02daa10b38d55fe2d81272
JunQuann <limjunquann@gmail.com>
2017-10-19 14:56:33 +0800

    Edited parsing of dp/ to make it non compulsory in addcommand

--- src/main/java/seedu/address/commons/util/ImagePathUtil.java
+++ src/main/java/seedu/address/commons/util/ImagePathUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.commons.util;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DP;
+import static seedu.address.ui.CommandBox.DEFAULT_DISPLAY_PIC;
 
 import seedu.address.ui.CommandBox;
 
@@ -11,19 +12,14 @@ import seedu.address.ui.CommandBox;
 public class ImagePathUtil {
 
     public static String requireFileChooser(String arguments, CommandBox commandBox) {
-        if (arguments.contains(PREFIX_DP.getPrefix())) {
-            arguments = appendDpPath(arguments, commandBox);
-        }
-        return arguments;
-    }
-
-    private static String appendDpPath(String arguments, CommandBox commandBox) {
         int prefixIndex = findPrefixPosition(arguments, PREFIX_DP.getPrefix(), 0);
         if (prefixIndex != -1) {
             StringBuilder sb = new StringBuilder((CharSequence) arguments);
             String selectedPath = commandBox.getDisplayPicPath();
             sb.insert(prefixIndex + PREFIX_DP.getPrefix().length(), selectedPath);
             arguments = sb.toString();
+        } else {
+            arguments += " " + PREFIX_DP + DEFAULT_DISPLAY_PIC;
         }
         return arguments;
     }
@@ -33,6 +29,5 @@ public class ImagePathUtil {
         return prefixIndex == -1 ? -1
                 : prefixIndex + 1; // +1 as offset for whitespace
     }
-
 
 }
