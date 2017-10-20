90f1bf2fcd31711e0d54e5fcf8535b3f20643847
tbhbhbh <tanbinghwang@gmail.com>
2017-10-20 21:42:48 +0800

    deleted redundant imports

--- src/main/java/seedu/address/logic/commands/TagDeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/TagDeleteCommand.java
@@ -1,25 +1,15 @@
 package seedu.address.logic.commands;
 
 import java.util.List;
-import java.util.Observable;
 
 import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.exceptions.TagNotFoundException;
 
-import javax.swing.text.html.HTML;
-
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.commands.EditCommand.MESSAGE_DUPLICATE_PERSON;
-import static seedu.address.model.tag.Tag.MESSAGE_TAG_CONSTRAINTS;
-import static seedu.address.model.tag.Tag.isValidTagName;
 
 /**
  * Deletes a tag identified using its tag name from the address book.
--- src/main/java/seedu/address/model/tag/exceptions/TagNotFoundException.java
+++ src/main/java/seedu/address/model/tag/exceptions/TagNotFoundException.java
@@ -3,5 +3,4 @@ package seedu.address.model.tag.exceptions;
 /**
  * Signals that the operation is unable to find the specified tag.
  */
-
 public class TagNotFoundException extends Exception {}
