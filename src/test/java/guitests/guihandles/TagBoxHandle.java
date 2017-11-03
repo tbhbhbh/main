57df4714fe86533729aa7b098a16594c87e5b26b
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-11-03 17:44:15 +0800

    Added a test to check if tagBox is clicked

--- src/test/java/guitests/guihandles/TagBoxHandle.java
+++ src/test/java/guitests/guihandles/TagBoxHandle.java
@@ -12,10 +12,23 @@ public class TagBoxHandle extends NodeHandle<Node> {
 
     private final Label tagLabel;
 
+    private boolean isTagBoxClicked = false;
+
     public TagBoxHandle(Node boxNode) {
         super(boxNode);
-
+        this.isTagBoxClicked = false;
         this.tagLabel = getChildNode(TAG_FIELD_ID);
+    }
+
+    // isTagBoxClicked returns true after guiRobot has click the rootNode
+    @Override
+    public void click() {
+        guiRobot.clickOn(getRootNode());
+        this.isTagBoxClicked = true;
+    }
+
+    public boolean isClicked() {
+        return isTagBoxClicked;
     }
 
     public String getTagName() {
--- src/test/java/seedu/address/ui/TagBoxTest.java
+++ src/test/java/seedu/address/ui/TagBoxTest.java
@@ -3,37 +3,58 @@ package seedu.address.ui;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static seedu.address.testutil.TypicalTags.CLASSMATES;
+import static seedu.address.testutil.TypicalTags.FACEBOOK;
+import static seedu.address.testutil.TypicalTags.FRIENDS;
+import static seedu.address.testutil.TypicalTags.GOOGLE;
+import static seedu.address.testutil.TypicalTags.OWE_MONEY;
 import static seedu.address.ui.testutil.GuiTestAssert.assertBoxDisplayTag;
 
 import org.junit.Test;
 
 import guitests.guihandles.TagBoxHandle;
 import seedu.address.model.tag.Tag;
-import seedu.address.testutil.TagBuilder;
 
 public class TagBoxTest extends GuiUnitTest {
+
+    private Tag testTag;
+    private TagBox testTagBox;
 
     @Test
     public void display() {
         // Label with a tag
-        Tag testTag = new TagBuilder().withSpecifiedTagName("alpha");
-        TagBox tagBox = new TagBox(testTag);
-        uiPartRule.setUiPart(tagBox);
+        testTag = GOOGLE;
+        testTagBox = new TagBox(testTag);
+        uiPartRule.setUiPart(testTagBox);
 
-        assertTagBoxDisplay(tagBox, testTag);
+        assertTagBoxDisplay(testTagBox, testTag);
 
         // Changes label to reflect another new tag
-        Tag anotherTag = new TagBuilder().withSpecifiedTagName("beta");
-        tagBox = new TagBox(anotherTag);
-        uiPartRule.setUiPart(tagBox);
+        Tag anotherTag = FACEBOOK;
+        testTagBox = new TagBox(anotherTag);
+        uiPartRule.setUiPart(testTagBox);
 
-        assertTagBoxDisplay(tagBox, anotherTag);
+        assertTagBoxDisplay(testTagBox, anotherTag);
+    }
+
+    @Test
+    public void mouseEventHandler_tagBoxIsClicked() {
+        testTag = OWE_MONEY;
+        testTagBox = new TagBox(testTag);
+        uiPartRule.setUiPart(testTagBox);
+
+        TagBoxHandle tagBoxHandle = new TagBoxHandle(testTagBox.getRoot());
+        tagBoxHandle.click();
+
+        guiRobot.waitForEvent(tagBoxHandle::isClicked);
+
+        assertTrue(tagBoxHandle.isClicked());
     }
 
     @Test
     public void equals() {
-        Tag tag = new TagBuilder().withSpecifiedTagName("test");
-        TagBox tagBox = new TagBox(tag);
+        testTag = FRIENDS;
+        TagBox tagBox = new TagBox(testTag);
 
         // same object -> returns true
         assertTrue(tagBox.equals(tagBox));
@@ -42,7 +63,7 @@ public class TagBoxTest extends GuiUnitTest {
         assertFalse(tagBox.equals(null));
 
         // different types -> returns false
-        Tag differentTag = new TagBuilder().withSpecifiedTagName("anotherTest");
+        Tag differentTag = CLASSMATES;
         assertFalse(tagBox.equals(new TagBox(differentTag)));
     }
 
