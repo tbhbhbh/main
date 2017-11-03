46a9ec9ec02d4374ca15981f461abcc2189b1c4e
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-11-03 23:44:20 +0800

    Include a new Person predicate

---
+++ src/main/java/seedu/address/model/person/PersonContainsBirthdayPredicate.java
@@ -0,0 +1,28 @@
+package seedu.address.model.person;
+
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Birthday} matches any of the birthday month given
+ */
+public class PersonContainsBirthdayPredicate implements Predicate<ReadOnlyPerson> {
+    private final String birthdayMonth;
+
+    public PersonContainsBirthdayPredicate(String birthdayMonth) {
+        this.birthdayMonth = birthdayMonth;
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        return birthdayMonth.equals(person.getBirthday().getBirthdayMonth());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PersonContainsBirthdayPredicate// instanceof handles nulls
+                && this.birthdayMonth.equals(((PersonContainsBirthdayPredicate) other)
+                .birthdayMonth)); // state check
+    }
+
+}
