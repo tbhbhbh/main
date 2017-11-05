950646a2239aa04f4dcda08d2b371fe82b70090a
JunQuann <limjunquann@gmail.com>
2017-11-05 22:57:57 +0800

    Fixed fatal bug when putting in invalid input for display pic

--- src/main/java/seedu/address/model/person/DisplayPic.java
+++ src/main/java/seedu/address/model/person/DisplayPic.java
@@ -1,6 +1,11 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.ui.CommandBox.DEFAULT_DISPLAY_PIC;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 
@@ -9,12 +14,21 @@ import seedu.address.commons.exceptions.IllegalValueException;
  */
 public class DisplayPic {
 
+    public static final String MESSAGE_DISPLAYPIC_CONSTRAINT = "When choosing the choice or display picture, ensure that you enter either dp/Y or dp/N";
     public final String displayPicPath;
 
     public DisplayPic(String displayPicPath) throws IllegalValueException {
         requireNonNull(displayPicPath);
         String trimmedDisplayPicPath = displayPicPath.trim();
+        if (!isValidPath(trimmedDisplayPicPath)) {
+            throw new IllegalValueException(MESSAGE_DISPLAYPIC_CONSTRAINT);
+        }
         this.displayPicPath = trimmedDisplayPicPath;
+    }
+
+    public static boolean isValidPath(String displayPicPath) {
+        File picture = new File(displayPicPath);
+        return displayPicPath.equals(DEFAULT_DISPLAY_PIC) || picture.exists();
     }
 
     @Override
@@ -28,7 +42,6 @@ public class DisplayPic {
                 || (other instanceof DisplayPic // instanceof handles nulls
                 && this.displayPicPath.equals(((DisplayPic) other).displayPicPath)); // state check
     }
-
 
     @Override
     public int hashCode() {
