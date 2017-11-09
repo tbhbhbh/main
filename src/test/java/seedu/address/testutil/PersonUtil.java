572c2dc8f9150ef8d4278aae55afc234a5df40ed
JunQuann <limjunquann@gmail.com>
2017-11-10 01:43:27 +0800

    Edited personUtil to remove duplicate codes

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -2,6 +2,7 @@ package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INSTAGRAM;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
@@ -25,13 +26,6 @@ public class PersonUtil {
     }
 
     /**
-     * Returns an add command string for adding the {@code person}.
-     */
-    public static String getAddInput(ReadOnlyPerson person) {
-        return AddCommand.COMMAND_WORD + " " + getPersonInput(person);
-    }
-
-    /**
      * Returns the part of command string for the given {@code person}'s details.
      */
     public static String getPersonDetails(ReadOnlyPerson person) {
@@ -43,24 +37,11 @@ public class PersonUtil {
         sb.append(PREFIX_BIRTHDAY + person.getBirthday().value + " ");
         sb.append(PREFIX_TWITTER + person.getTwitterName().value + " ");
         sb.append(PREFIX_INSTAGRAM + person.getInstagramName().value + " ");
-        person.getTags().stream().forEach(
-            s -> sb.append(PREFIX_TAG + s.tagName + " ")
-        );
-        return sb.toString();
-    }
-
-    public static String getPersonInput(ReadOnlyPerson person) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(PREFIX_NAME + person.getName().fullName + " ");
-        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
-        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
-        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        sb.append(PREFIX_BIRTHDAY + person.getBirthday().value + " ");
-        sb.append(PREFIX_TWITTER + person.getTwitterName().value + " ");
-        sb.append(PREFIX_INSTAGRAM + person.getInstagramName().value + " ");
+        sb.append(PREFIX_DP + person.getDisplayPic().toString() + " ");
         person.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
         return sb.toString();
     }
 }
+
